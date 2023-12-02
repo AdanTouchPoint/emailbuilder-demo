3472c62e2b6b67fe59fb4882313741263ea44ce3
AdanTouchPoint <adan.mijangos@touchpointmarketing.mx>
2023-12-01 19:34:59 -0600

    fix representatives endpoint

--- app/assets/petitions/fetchRepresentatives.js
+++ app/assets/petitions/fetchRepresentatives.js
@@ -2,21 +2,13 @@ import { fetchData } from "./fetchData";
 
 const fetchRepresentatives = async (petitionMethod, backendURLBase, endpoint, clientId, params = '', setMp, setSenator, setShowLoadSpin, setShowList,sendMany,setAllDataIn) => {
     const datos = await fetchData(petitionMethod, backendURLBase, endpoint, clientId, params)
-    if(sendMany === "Si") {
-      const emails = await mailerExtracter(datos.data)
-      console.log(emails)
-      setAllDataIn(emails)
-      setMp(datos.data)
-      setShowLoadSpin(false)
-      setShowList(false)
-      return
-     }
+   console.log(datos)
     let query = datos.data;
     let fill = await query.map((el) => {
       return el[0];
     });
-    setMp(fill);
-    setSenator(datos.statesFilter)
+    setMp(datos.mps);
+    setSenator(datos.data)
     setShowLoadSpin(false)
     setShowList(false)
 
